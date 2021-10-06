c9ae3632b9145fe5dbea94682ba7a5b340b8d7c1
OlliMm <62538897+OlliMm@users.noreply.github.com>
2021-10-06 19:33:45 +0300

    push

---
+++ lukujenjarjestys/Lukujenjarjestys/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Lukujenjarjestys
+{
+    partial class LukujenJarjestys
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UusilukuTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna luku (-999 lopetus):";
+            // 
+            // UusilukuTB
+            // 
+            this.UusilukuTB.Location = new System.Drawing.Point(266, 47);
+            this.UusilukuTB.Name = "UusilukuTB";
+            this.UusilukuTB.Size = new System.Drawing.Size(150, 31);
+            this.UusilukuTB.TabIndex = 1;
+            this.UusilukuTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(30, 107);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(72, 25);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // LukujenJarjestys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 161);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.UusilukuTB);
+            this.Controls.Add(this.label1);
+            this.Name = "LukujenJarjestys";
+            this.Text = "Lukujenjarjestys";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UusilukuTB;
+        private System.Windows.Forms.Label VastausLB;
+    }
+}
+
